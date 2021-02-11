095bcbe85f2335e1e90682dedc4a46558244c97f
micartiaga <micaela.artiaga@gmail.com>
2021-02-11 00:14:16 -0300

    models 2

--- database/migrations.js
+++ database/migrations.js
@@ -1,11 +1,12 @@
 const User = require('../models/User');
 const Product = require('../models/Product');
-const Order = require('../models/Order')
+const Order = require('../models/Order');
 
 (async () => {
 
     await User.sync();
     await Product.sync();
+    await Order.sync();
    
 })();
 
