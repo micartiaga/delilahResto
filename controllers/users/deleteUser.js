865c3b1dc3ea13e22f42b3f87842e28634980cbc
micartiaga <micaela.artiaga@gmail.com>
2021-02-12 17:08:00 -0300

    usuario controllers ok

--- controllers/users/deleteUser.js
+++ controllers/users/deleteUser.js
@@ -1,10 +1,28 @@
+require('dotenv/config');
 const express = require('express');
 const route = express.Router();
-const connection = require('../../database/connection-DB');
+const User = require('../../models/User');
 
 
-route.get("/", async (req, res)=>{
-    res.send("Buenas borralo que sos admin");
+route.delete("/", async (req, res) => {
+
+    try {
+        let username = req.body.username;
+        let usuario = await User.findOne({ where: {username: username }});
+        // VALIDANDO SI EXISTE EL USUARIO
+        if (!usuario) {
+            return res.status(404).send({ Error: error.message })
+        }
+        
+        usuario.destroy();
+        return res.sendStatus(200);
+
+    }
+    catch (error) {
+        console.log({ message: error.message });
+        return res.status(404).send({ Error: error.message })
+    }
+
 });
 
 module.exports = route;
